96965092292fcef93d852e65cbd7605bc21825ab
ankita2399 <86045136+ankita2399@users.noreply.github.com>
2021-06-22 09:54:48 +0530

    accept name from user and print that name.

---
+++ info.c
@@ -0,0 +1,11 @@
+/*Write a program which accept name from user and print that name.
+*/
+#include<stdio.h>
+int main()
+{
+    char Name[30];
+    printf("Please Enter Full Name:");
+    scanf("%s",&Name);
+    printf("Your name is %s", Name);
+    return 0;
+}
